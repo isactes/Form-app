490e75a3bcc9363d720019faee4b3da62081163a
isactes <fidel.renovato@gmail.com>
2022-05-06 19:29:44 -0500

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Form-app';
+
+  public "cards":Array<any> = [];
+  ngOnInit(): void {
+    this.cards = [
+      {
+        title:'videobox1',
+        subtitle:'subbox1'
+      },
+      {
+        title:'videobox2',
+        subtitle:'subbox2'
+      },
+      {
+        title:'videobox3',
+        subtitle:'subbox3'
+      }
+    ];
+  }
 }
